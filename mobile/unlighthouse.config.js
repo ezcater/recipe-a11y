2eec8f491f721d99e8dc6cf200086d468982c3c7
danidewitt <danidewitt@gmail.com>
2022-12-01 01:02:40 -0500

    add mobile and home pages

---
+++ mobile/unlighthouse.config.js
@@ -0,0 +1,9 @@
+export default {
+  site: "https://ezcater.github.io/recipe",
+  scanner: {
+    device: "mobile",
+    skipJavascript: false,
+    dynamicSampling: false,
+  },
+  routerPrefix: "/recipe-a11y/mobile",
+};
---
+++ web/unlighthouse.config.js
@@ -0,0 +1,9 @@
+export default {
+  site: "https://ezcater.github.io/recipe",
+  scanner: {
+    device: "desktop",
+    skipJavascript: false,
+    dynamicSampling: false,
+  },
+  routerPrefix: "/recipe-a11y/web",
+};
